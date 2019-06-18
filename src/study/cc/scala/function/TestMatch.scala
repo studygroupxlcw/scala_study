ed5da23ef091530a1c24f5e97e5902bdd1729dca
CC <1035087119@qq.com>
2019-06-18 10:13:01 +0800

    整合

--- src/study/cc/scala/function/TestMatch.scala
+++ src/study/cc/scala/function/TestMatch.scala
@@ -1,14 +1,46 @@
 package study.cc.scala.function
 
-object TestMatch {
+object TestSwitchCase {
   def main(args: Array[String]): Unit = {
-    List("Sunday", 1, "Monday", 1.0,'h').map(matchMethod(_)).foreach(println)
+    println(selfMatch("javase"))
   }
 
-  def matchMethod(day: Any): String = day match {
-    case "Sunday" => "Eat...PlayGame..."
-    case 1 => "这是1，数值类型"
-    case "Monday" => "work...work..."
-    case _ => "不知道什么类型，通配符"
+  def selfMatch(x: String): String = x match {
+    case "javase" => {
+      var i = 3
+      if (i > 3) {
+        "Java开发标准版"
+      } else {
+        "Java开发标准版"
+      }
+    }
+    case "javaee" => "Java开发企业版"
+    case _ => "其他Java版本"
   }
+
+  def selfMatch(x: Any): Any = x match {
+    case "one" => 1
+    case 2 => "two"
+    case "III" => "希腊数字3"
+    case _ => "其他"
+  }
+
+  def selfMatch(x: Int): String = x match {
+    case _ if x < 12 => "幼年"
+    case _ if x < 18 => "未成年"
+    case _ if x >= 18 && x < 60 => "成年"
+    case _ => "已经步入老年"
+  }
+
+  def selfMatchObj(x: Any): Any = x match {
+    case _: Int => "传入的是整型"
+    case _: String => "传入的是字符串"
+    case _: TestPatternMatch => "传入的类型是TestPatternMatch"
+    case _ => "不知道什么类型"
+  }
+
+}
+
+class TestPatternMatch {
+
 }
