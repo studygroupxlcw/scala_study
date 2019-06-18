69762df46e8cf5128627f752f5f1b29854e2c860
CC <1035087119@qq.com>
2019-06-18 10:18:18 +0800

    整合

--- src/study/cc/scala/function/FunctionTest.scala
+++ src/study/cc/scala/function/FunctionTest.scala
@@ -2,12 +2,23 @@ package study.cc.scala.function
 
 object FunctionTest {
   def functionName(arg: String): String = {
-    // function body
     println(arg)
     "Hello"
   }
 
+  // 没有参数的方法可以叫做过程，括号可以省略 最后一行是返回值
+  def function: String = {
+    "无参方法"
+  }
+
+  // 没有返回值的函数也可以省略,建议加上:Unit 返回类型方便阅读，形式上看起来就像一个闭包
+  def functiontest = {
+    println("无参无返回值")
+  }
+
   def main(args: Array[String]): Unit = {
     println(functionName("test"))
+    println(function)
+    functiontest
   }
 }
